264cea93232216395db97616c8876bd362e08188
Pablo <orti90@gmail.com>
2017-06-18 20:36:05 +0200

    v.0.0.1 Add firebase environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebase: {
+    apiKey: "AIzaSyBf2l5eotzkOAKLAKntWkb5TamLweXhKQQ",
+    authDomain: "taskcounter-a703a.firebaseapp.com",
+    databaseURL: "https://taskcounter-a703a.firebaseio.com",
+    projectId: "taskcounter-a703a",
+    storageBucket: "taskcounter-a703a.appspot.com",
+    messagingSenderId: "314613422099"
+  }
 };
